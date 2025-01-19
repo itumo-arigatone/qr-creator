34431f99a5b7e74449b1f3fe6e949fa3f4ac1fdb
itsumoarigatone <soganokasegi0825@gmail.com>
2025-01-19 18:01:16 +0900

    add google adsense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GoogleAdsense from "@/app/_components/GoogleAdsense";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
+      <GoogleAdsense pId={process.env.GOOGLE_ADSENSE_ID || ""} />
     </html>
   );
 }
