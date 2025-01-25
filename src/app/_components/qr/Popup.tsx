819b67001296a77cdf65cdebeb5e6ed4659c0669
itsumoarigatone <soganokasegi0825@gmail.com>
2025-01-26 00:46:08 +0900

    close button

--- src/app/_components/qr/Popup.tsx
+++ src/app/_components/qr/Popup.tsx
@@ -12,13 +12,18 @@ const Popup = ({ isHideTop, ref }: Props) => {
   const [isDismissed, setDismissed] = useState(false);
 
   return (
-    isHideTop &&
-    !isDismissed && (
-      <div className={`${isHideTop ? styles.popup : ""}`}>
-        <div className={`${styles.preview}`} ref={ref} />
-        <div onClick={() => setDismissed(true)}>閉じる</div>
-      </div>
-    )
+    <div className={styles.popup}>
+      {(isHideTop && !isDismissed && (
+        <>
+          <div
+            className={`${styles.preview} ${styles.popup_preview}`}
+            ref={ref}
+          />
+          <div onClick={() => setDismissed(true)}>閉じる</div>
+        </>
+      )) ||
+        (isDismissed && <div onClick={() => setDismissed(false)}>再表示</div>)}
+    </div>
   );
 };
 
