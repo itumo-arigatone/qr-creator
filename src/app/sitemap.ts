0334c1dad07e11d07e1cf935bfeae840101a60b6
itsumoarigatone <soganokasegi0825@gmail.com>
2025-02-02 02:32:14 +0900

    サイトマップ追加

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,6 +6,10 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: "https://qr.itsumoarigatone.com",
       lastModified: "2025-01-22T00:00:00.000Z",
     },
+    {
+      url: "https://qr.itsumoarigatone.com/how-to",
+      lastModified: "2025-02-02T02:32:00.000Z",
+    },
   ];
   // 配列の結合
   return [...list];
