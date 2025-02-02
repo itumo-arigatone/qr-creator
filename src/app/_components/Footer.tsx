644699b33c6c0c24c7f4af37afac88ffca875e73
itsumoarigatone <soganokasegi0825@gmail.com>
2025-02-02 14:32:22 +0900

    プラポリ追加

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,9 +1,18 @@
 import styles from "@/app/styles/footer.module.css";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      Copyright © 2025 Itsumoarigatone. All Rights Reserved.
+      <Link
+        href="https://www.itsumoarigatone.com/policy/"
+        className={styles.link}
+      >
+        プライバシーボリシー
+      </Link>
+      <div className={styles.copyright}>
+        Copyright © 2025 Itsumoarigatone. All Rights Reserved.
+      </div>
     </footer>
   );
 };
